afc0e378e676b4f5efbadb61662dd2fcaffaefdf
bhavesh <bhavesh@idroidsolutions.in>
2015-07-24 18:33:21 +0530

    properties practice

--- swift_playground/Properties.playground/section-1.swift
+++ swift_playground/Properties.playground/section-1.swift
@@ -2,4 +2,163 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//Stored Properties
+
+struct  FixedLengthRange {
+    var firstValue:Int
+    let length:Int
+}
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+rangeOfThreeItems.firstValue=6
+//rangeOfThreeItems.length=8
+
+let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
+//rangeOfFourItems.firstValue=6
+
+// Lazy Stored Properties
+
+class DataImporter {
+    /*
+    DataImporter is a class to import data from an external file.
+    The class is assumed to take a non-trivial amount of time to initialize.
+    */
+    var fileName = "data.txt"
+    // the DataImporter class would provide data importing functionality here
+}
+
+
+class DataManager {
+    lazy var importer = DataImporter()
+    var data = [String]()
+    // the DataManager class would provide data management functionality here
+}
+
+
+let manager = DataManager()
+manager.data.append("Some data")
+manager.data.append("Some more data")
+
+println(manager.importer.fileName)
+
+
+
+
+//Computed Properties
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set(newCenter) {
+            origin.x = newCenter.x - (size.width / 2)
+            origin.y = newCenter.y - (size.height / 2)
+        }
+    }
+
+}
+
+
+var square = Rect(origin: Point(x: 0.0, y: 0.0),size: Size(width: 10.0, height: 10.0))
+
+let initialSquareCenter = square.center
+square.center = Point(x: 15.0, y: 15.0)
+println("square.origin is now at (\(square.origin.x), \(square.origin.y))")
+
+
+
+
+//Shorthand Setter Declaration
+
+struct AlternativeRect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set {
+            origin.x = newValue.x - (size.width / 2)
+            origin.y = newValue.y - (size.height / 2)
+        }
+    }
+}
+
+
+
+//Read-Only Computed Properties
+
+struct Cuboid {
+    var width = 0.0, height = 0.0, depth = 0.0
+    var volume: Double {
+        return width * height * depth
+    }
+}
+let fourByFiveByTwo = Cuboid(width: 4.0, height: 5.0, depth: 2.0)
+println("the volume of fourByFiveByTwo is \(fourByFiveByTwo.volume)")
+// prints "the volume of fourByFiveByTwo is 40.0"
+
+
+//Property Observers
+
+
+class StepCounter {
+    var totalSteps: Int = 0 {
+        willSet(newTotalSteps) {
+            println("About to set totalSteps to \(newTotalSteps)")
+        }
+        didSet {
+            if totalSteps > oldValue  {
+                println("Added \(totalSteps - oldValue) steps")
+            }
+        }
+    }
+}
+let stepCounter = StepCounter()
+stepCounter.totalSteps = 200
+// About to set totalSteps to 200
+// Added 200 steps
+stepCounter.totalSteps = 360
+// About to set totalSteps to 360
+// Added 160 steps
+stepCounter.totalSteps = 896
+
+//Global and Local Variables
+
+
+
+
+/*
+1. stored properties
+2. Lazy Stored Properties
+3. computed properties
+4. typed properties
+5. Property Observers
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
